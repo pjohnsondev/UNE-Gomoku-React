dcdaadbd3c5d923662eb362b142f7911ee5950ff
Paul Johnson <pauljohnson@Pauls-MacBook-Pro.local>
2022-07-27 19:59:15 +0900

    add button and game selection logic to home, remove drop down board sizes due to dev issues

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,30 @@
 import style from "./home.module.css"
 import React, {useState} from "react" 
 import { CSSTransition } from "react-transition-group"
+import { Button } from 'semantic-ui-react'
 
 const smallestBoard = 5
 const LargestBoard = 19
 export const gameSizes = [...Array(LargestBoard - smallestBoard + 1).keys()].map( (index) => index + smallestBoard)
 
 export default function Home(props: any) {
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(true)
+    const [selected, setSelected] = useState(false)
+    const buttonText = "Start"
+    let gameChoice: number
+
+    function renderBoard(props: number){
+      return(
+        console.log(props)
+      )
+    }
     
     function GameSize(props: any){
       return(
         <CSSTransition in={open === true } unmountOnExit timeout={500}> 
-        <li className={style['game-size']} key={props.gameSize}>
+        <li className={style['game-size']} key={props.gameSize} onClick={() => selected? setSelected(!selected) : gameChoice = props.gameSize}>
           <a href="#">
             <span>{props.gameSize}x{props.gameSize}</span>
-            {props.children}
           </a>
         </li>
         </CSSTransition>
@@ -24,16 +33,22 @@ export default function Home(props: any) {
 
     function GameSizes(){
       return (
-        <ul className={style['game-sizes']} onClick={() => setOpen(!open)}> 
-        <span>Select Game Size</span>
-        {gameSizes.map((size) => (
-          <GameSize gameSize = {size}/> 
-        ))}
-      </ul>
+        <div /*onClick={() => setOpen(!open)}*/>
+          <ul className={style['game-sizes']}> 
+            <span>Select Game Size</span>
+            {gameSizes.map((size) => (
+              <GameSize gameSize = {size} key={size}/> 
+            ))}
+          </ul>
+        </div>
       )
     }
 
     return (
-      <GameSizes/>
+      <>
+        <GameSizes/>
+        <Button primary onClick={() => renderBoard(gameChoice)}>{buttonText}</Button>
+      </>
+
     )
   }
